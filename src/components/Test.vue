90eda886ef0866689ac49309db04d6f885b09590
tantingqiang <tantingqiang@viomi.com.cn>
2020-04-30 17:42:18 +0800

    feat: add login componet

---
+++ src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>text</p>
+    <div>
+      <router-link to="/dbs">dbs</router-link>
+      <button @click="go1">gogogogo</button>
+    </div>
+    <div id="systemContainer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+    go1 () {
+      window.history.pushState('data', 'title', 'dbs');
+    }
+  }
+}
+</script>
+
+<style lang='less'>
+#systemContainer {
+  height: 500px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import { registerMicroApps, start } from 'qiankun'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+registerMicroApps([
+  {
+    name: 'dbs123',
+    entry: '//crm-web-test.viomi.com.cn/index.html',
+    container: '#systemContainer',
+    activeRule: '/dbs'
+  }
+])
+// loadMicroApp(
+//   {
+//     name: 'app',
+//     entry: '//localhost:8010',
+//     container: '#systemContainer',
+//   }
+// );
+start()
